596ffaa02a50e8f54cc40c0df263f619186a0aec
httpjamesm <github@httpjames.space>
2024-03-09 11:51:59 -0500

    feat: replace stackoverflow and exchange links

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 
 require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dlclark/regexp2 v1.7.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
@@ -25,6 +26,8 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/ugorji/go/codec v1.2.8 // indirect
 	golang.org/x/crypto v0.4.0 // indirect
 	golang.org/x/net v0.7.0 // indirect
@@ -32,4 +35,5 @@ require (
 	golang.org/x/text v0.7.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ src/utils/links_test.go
@@ -0,0 +1,49 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+var sampleInput = `<div class="d-flex fd-column fw-nowrap">
+	<div class="d-flex fw-nowrap">
+		<div class="flex--item wmn0 fl1 lh-lg">
+			<div class="flex--item fl1 lh-lg">
+					<div>
+						<b>This question already has answers here</b>:
+
+					</div>
+			</div>
+		</div>
+	</div>
+			<div class="flex--item mb0 mt4">
+				<a href="/questions/55083952/is-it-possible-to-populate-a-large-set-at-compile-time" dir="ltr">Is it possible to populate a large set at compile time?</a>
+					<span class="question-originals-answer-count">
+						(3 answers)
+					</span>
+			</div>
+			<div class="flex--item mb0 mt4">
+				<a href="/questions/27221504/how-can-you-make-a-safe-static-singleton-in-rust" dir="ltr">How can you make a safe static singleton in Rust?</a>
+					<span class="question-originals-answer-count">
+						(5 answers)
+					</span>
+			</div>
+			<div class="flex--item mb0 mt4">
+				<a href="https://security.stackexchange.com/questions/25371/brute-force-an-ssh-login-that-has-only-a-4-letter-password" dir="ltr">Brute-force an SSH-login that has only a 4-letter password</a>
+					<span class="question-originals-answer-count">
+						(9 answers)
+					</span>
+			</div>
+		<div class="flex--item mb0 mt8">Closed <span title="2020-01-29 14:28:42Z" class="relativetime">4 years ago</span>.</div>
+</div>`
+
+func TestReplaceStackOverflowLinks(t *testing.T) {
+	replacedLinks := ReplaceStackOverflowLinks(sampleInput)
+
+	fmt.Println(replacedLinks)
+
+	assert.False(t, strings.Contains(replacedLinks, "stackoverflow.com"))
+	assert.False(t, strings.Contains(replacedLinks, "stackexchange.com"))
+}
